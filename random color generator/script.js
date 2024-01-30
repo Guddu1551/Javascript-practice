b86c6439347b1e498a9ae0d9831000b08baa8921
Guddu1551 <115723402+Guddu1551@users.noreply.github.com>
2024-01-30 13:39:43 +0530

    Add files via upload

---
+++ random color generator/script.js
@@ -0,0 +1,15 @@
+console.log("random color generating.....")
+
+let boxes = document.querySelector(".container").children
+
+function getRandomColor(){
+        let val1 = Math.random()*255
+        let val2 = Math.random()*255
+        let val3 = Math.random()*255
+        return `rgb(${val1},${val2},${val3})`
+}
+
+Array.from(boxes).forEach(e =>{
+        e.style.backgroundColor = getRandomColor()
+        e.style.color = getRandomColor()
+})
